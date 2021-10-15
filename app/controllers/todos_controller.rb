222b0a7bf689ffcde023382826cd1225d8c325b7
David Watts <davidwatts11@gmail.com>
2021-10-15 12:28:46 +1000

    initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,12 @@
+class TodosController < ApplicationController
+    def index
+        @todo_items = TodoItem.all
+    end
+
+    def new
+    end
+
+    def show
+        @todo_item = TodoItem.find(params[:id])
+    end
+end
